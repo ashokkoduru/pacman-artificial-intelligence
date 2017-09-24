1b9e1cc2df23ef35baeaa43cb5667cf673fe39ef
Ashok Koduru <ashok.koduru9@gmail.com>
2017-09-24 15:04:35 -0400

    Started corners problem

--- searchAgents.py
+++ searchAgents.py
@@ -288,6 +288,7 @@ class CornersProblem(search.SearchProblem):
         # Please add any code here which you would like to use
         # in initializing the problem
         "*** YOUR CODE HERE ***"
+        self.corners_not_explored = self.corners
 
     def getStartState(self):
         """
@@ -295,14 +296,18 @@ class CornersProblem(search.SearchProblem):
         space)
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.startingPosition, self.corners_not_explored
+
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        for each in state[1]:
+            if len(each) != 3:
+                return False
+        return True
 
     def getSuccessors(self, state):
         """
@@ -325,6 +330,14 @@ class CornersProblem(search.SearchProblem):
             #   hitsWall = self.walls[nextx][nexty]
 
             "*** YOUR CODE HERE ***"
+            x, y =  state[0]
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            hitsWall = self.walls[nextx][nexty]
+            if not hitsWall:
+                return
+
+
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
